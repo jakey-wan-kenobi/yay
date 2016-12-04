4c5e080ee6a45a3cb1bb548d314667aefce969c2
jakey-wan-kenobi <jacobrobertallen@gmail.com>
2016-12-03 18:10:26 -0800

    Wrote HTTPS for server. Working in production.

---
+++ server.js
@@ -0,0 +1,42 @@
+let express = require('express')
+let app = express()
+let https = require('https')
+let http = require('http')
+
+/* ******************************************* LETS-ENCRYPT SSL SETUP *********************************************/
+
+// Returns an instance of node-letsencrypt with additional helper methods
+let lex = require('letsencrypt-express').create({
+  // NOTE: Set to https://acme-v01.api.letsencrypt.org/directory in production,
+  // https://acme-staging.api.letsencrypt.org/directory in staging
+  server: 'https://acme-v01.api.letsencrypt.org/directory',
+  challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) },
+  store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' }),
+  approveDomains: approveDomains
+})
+
+// This is where you check your database and associated email addresses with domains and agreements and such
+function approveDomains (opts, certs, cb) {
+  // The domains being approved for the first time are listed in opts.domains
+  // Certs being renewed are listed in certs.altnames
+  if (certs) {
+    opts.domains = certs.altnames
+  } else {
+    opts.email = 'jake@hintsygifts.com'
+    opts.agreeTos = true
+  }
+
+  // NOTE: you can also change other options such as `challengeType` and `challenge`
+  // opts.challengeType = 'http-01';
+  // opts.challenge = require('le-challenge-fs').create({});
+
+  cb(null, { options: opts, certs: certs })
+}
+
+app.get('/', function (req, res) {
+  console.log('Hello world!')
+  res.send('Hello World!')
+})
+
+http.createServer(lex.middleware(require('redirect-https')())).listen(80)
+https.createServer(lex.httpsOptions, lex.middleware(app)).listen(443)
