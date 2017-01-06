0eea7a4906c1a44bf66fcda8cd65aeabda9551c4
jakey-wan-kenobi <jacobrobertallen@gmail.com>
2017-01-05 20:52:05 -0800

    Promisified some credit card functionality to pass data back when successful. Allows us to be more accurate about success/fail.

--- server.js
+++ server.js
@@ -419,71 +419,94 @@ api.route('/save-order-details')
     // Check whether stripe_id exists for this user, and then decide how to process data recieved (may be credit card token from Stripe, or shipping address data)
     co(function * () {
       let stripeIDCheck = yield _checkForStripeID(decodedJWT)
-      _processDataConditionally(stripeIDCheck, creditCard, decodedJWT, shipping)
-      // TODO: Turn these into async and then return status from result
-      res.sendStatus(200)
+      let customer = yield _processDataConditionally(stripeIDCheck, creditCard, decodedJWT, shipping)
+      // Send the credit card data back to client (for success/fail message) TODO: What happens if we failed?
+      res.send(customer)
     })
   })
 
 // Decide whether to add new Stripe customer, or update an existing one
 function _processDataConditionally (stripeCheck, card, auth, shipping) {
-  // If team already has a stripe_id, add this card via Stripe API
-  if (stripeCheck.has_stripe_id === 'yes') {
-    _saveToExistingStripeCustomer(stripeCheck, card, auth, shipping)
-  }
-  // If team does not already have a stripe_id, create this customer via Stripe API
-  if (stripeCheck.has_stripe_id === 'no') {
-    _createNewStripeCustomer(card, auth, shipping)
-  }
+  return new Promise(function (resolve, reject) {
+    // If team already has a stripe_id, add this card via Stripe API, then pass customer when we're done.
+    co(function * () {
+      if (stripeCheck.has_stripe_id === 'yes') {
+        const customer = yield _saveToExistingStripeCustomer(stripeCheck, card, auth, shipping)
+        // TODO TOGETHER: When this function is done, res.send the credit card data back to the view.
+        resolve(customer)
+        return customer
+      }
+      // If team does not already have a stripe_id, create this customer via Stripe API. Then pass customer when we're done.
+      if (stripeCheck.has_stripe_id === 'no') {
+        const customer = yield _createNewStripeCustomer(card, auth, shipping)
+        resolve(customer)
+        return customer
+      }
+    })
+  })
 }
 
-// Create a new Stripe customer and save to Firebase, under that user's team_id/user_id
+// Create a new Stripe customer and save to Firebase, under that user's team_id/user_id, then reject or resolve with customer data or error when we're done.
 function _createNewStripeCustomer (card, auth, shipping) {
-  stripe.customers.create({
-    description: 'Slack team ' + auth.team_id + ', User ' + auth.user_id,
-    metadata: {
-      user_id: auth.user_id,
-      team_id: auth.team_id
-    },
-    // Note: The following two keys are conditional, because we use this route to handle both credit card and address.
-    source: card ? card.id : undefined, // Token obtained with Stripe.js
-    shipping: shipping ? {
-      address: shipping,
-      name: 'Team' // TODO: I'm hacking this in as a default just becuase Stripe requires it to save address
-    } : undefined
-    // email: TODO: Store the primary user's email in the JWT so we can add it here for receipts etc.
-  }, function (err, customer) {
-    // Asynchronously called
-    if (err) {
-      // TODO: Handle error
-      console.log(err)
-    }
-    // TODO: Save stripe ID to Firebase
-    let account = db.ref('/slack_accounts_users/' + auth.team_id + '/' + auth.user_id)
-    account.update({
-      stripe_id: customer.id
+  return new Promise(function (resolve, reject) {
+    stripe.customers.create({
+      description: 'Slack team ' + auth.team_id + ', User ' + auth.user_id,
+      metadata: {
+        user_id: auth.user_id,
+        team_id: auth.team_id
+      },
+      // Note: The following two keys are conditional, because we use this route to handle both credit card and address.
+      source: card ? card.id : undefined, // Token obtained with Stripe.js
+      shipping: shipping ? {
+        address: shipping,
+        name: 'Team' // TODO: I'm hacking this in as a default just becuase Stripe requires it to save address
+      } : undefined
+      // email: TODO: Store the primary user's email in the JWT so we can add it here for receipts etc.
+    }, function (err, customer) {
+      // Asynchronously called
+      if (err) {
+        console.log(err)
+        reject(err)
+        return err
+      }
+      // TODO: Save stripe ID to Firebase
+      let account = db.ref('/slack_accounts_users/' + auth.team_id + '/' + auth.user_id)
+      account.update({
+        stripe_id: customer.id
+      }).then(function () {
+        resolve(customer)
+        return customer
+      }).catch(function (err) {
+        // NOTE: Didn't test whether this works.
+        reject(err)
+        return err
+      })
     })
   })
 }
 
 // Update an existing Stripe customer with the credit card they've just added.
 function _saveToExistingStripeCustomer (stripeCheck, card, auth, shipping) {
-  // console.log(shipping, stripeCheck.stripe_id)
-  stripe.customers.update(stripeCheck.stripe_id, {
-    // Note: The following two keys are conditional, because we use this route to handle both credit card and address.
-    source: card ? card.id : undefined,
-    shipping: shipping ? {
-      address: shipping,
-      name: 'Team' // TODO: I'm hacking this in as a default just becuase Stripe requires it to save address
-    } : undefined
-  }, function (err, customer) {
-    // TODO: Handle error
-    if (err) {
-      console.log(err)
-      return
-    }
-    // TODO: Sucess message. Not saving to Firebase because we saved default card to Stripe. We'll just poll Stripe API if we need that data.
-    // console.log('success!', customer)
+  // TODO TOGETHER: Make this return a promise so we can yield to it above
+  return new Promise(function (resolve, reject) {
+    // console.log(shipping, stripeCheck.stripe_id)
+    stripe.customers.update(stripeCheck.stripe_id, {
+      // Note: The following two keys are conditional, because we use this route to handle both credit card and address.
+      source: card ? card.id : undefined,
+      shipping: shipping ? {
+        address: shipping,
+        name: 'Team' // TODO: I'm hacking this in as a default just becuase Stripe requires it to save address
+      } : undefined
+    }, function (err, customer) {
+      // TODO: Handle error
+      if (err) {
+        console.log(err)
+        reject(err)
+        return err
+      }
+      resolve(customer)
+      return customer
+    })
   })
 }
 
